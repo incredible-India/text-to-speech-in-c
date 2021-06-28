94413fa5887875a285ec4d612f911515d99cfe44
Himanshu sharma <iamonlyhimanshusharma@gmail.com>
2021-06-28 22:29:26 +0530

    added all initial files

---
+++ main.c
@@ -0,0 +1,24 @@
+/*This project will generate the python code automatically, It is only for the entertainment purpose..
+ Himanshu Sharma 4th Sem B.E  */
+
+ //main File
+
+ //Including All headers files
+ //built-in headers files
+ #include<stdio.h>
+ #include<conio.h>
+ #include<string.h>
+ #include<windows.h>
+ #include<stdlib.h>
+
+ //User define header file
+ #include"./header Files/impFNS.h"
+
+ //function declaration
+
+ int main()
+ {
+     ExitFn();
+
+ }
+
